2818c298ca88b8002e9ce0c03b13a564c1435cc9
簡文翊 <john27306666@yahoo.com.tw>
2025-12-18 19:28:08 +0800

    追加Swagger 設定

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -5,11 +5,32 @@ using WebApplication3.Models;
 
 namespace WebApplication3.Views.API
 {
+
     [ApiController]
     [Route("API")]
+    [Produces("application/json")]
     public class APIController : ControllerBase
     {
+        /// <summary> SQL的查詢功能  </summary>
+        /// <param name="select"></param>
+        /// <returns> 查詢資料庫用戶的名稱 </returns>
+        /// <remarks>
+        /// 範例為：
+        /// POST /API/ApiSelect{
+        /// "userId": 0,
+        /// "name": "john",
+        /// "oldName": "",
+        /// "newName": "",
+        /// "phone": "",
+        /// "address": "",
+        /// "sex": 0
+        /// }
+        /// </remarks>
+        /// <response code="200">查詢成功</response>
+        /// <response code="400">資料庫沒有資料</response>
         [HttpPost("ApiSelect")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult ApiSelect([FromBody] DBClass data)
         {
             bool success = false;
@@ -34,7 +55,27 @@ namespace WebApplication3.Views.API
                 return BadRequest(new { status = "failure", message = "資料取得失敗" });
         }
 
+
+        /// <summary> SQL的新增功能  </summary>
+        /// <param name="insert"></param>
+        /// <returns> 新增資料庫用戶 </returns>
+        /// <remarks>
+        /// 範例為：
+        /// POST /API/ApiInsert{
+        /// "userId": 0,
+        /// "name": "john",
+        /// "oldName": "",
+        /// "newName": "",
+        /// "phone": "0987654321",
+        /// "address": "aaaaaaaaaaaaaaa",
+        /// "sex": 0
+        /// }
+        /// </remarks>
+        /// <response code="201">新增成功</response>
+        /// <response code="400">資料庫無法新增</response>
         [HttpPost("ApiInsert")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult ApiInsert([FromBody] DBClass data)
         {
             bool success = false;
@@ -46,7 +87,26 @@ namespace WebApplication3.Views.API
                 return BadRequest(new { status = "failure", message = "資料新增失敗" });
         }
 
+        /// <summary> SQL的修改功能  </summary>
+        /// <param name="update"></param>
+        /// <returns> 修改資料庫用戶的名稱 </returns>
+        /// <remarks>
+        /// 範例為：
+        /// POST /API/ApiUpdate{
+        /// "userId": 0,
+        /// "name": "",
+        /// "oldName": "john",
+        /// "newName": "mary",
+        /// "phone": "",
+        /// "address": "",
+        /// "sex": 0
+        /// }
+        /// </remarks>
+        /// <response code="200">修改成功</response>
+        /// <response code="400">用戶名稱修改失敗</response>
         [HttpPut("ApiUpdate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult ApiUpdate([FromBody] DBClass data)
         {
             bool success = false;
@@ -58,7 +118,26 @@ namespace WebApplication3.Views.API
                 return BadRequest(new { status = "failure", message = "資料更新失敗" });
         }
 
+        /// <summary> SQL的刪除功能  </summary>
+        /// <param name="delete"></param>
+        /// <returns> 刪除資料庫用戶 </returns>
+        /// <remarks>
+        /// 範例為：
+        /// POST /API/ApiDelete{
+        /// "userId": 0,
+        /// "name": "john",
+        /// "oldName": "",
+        /// "newName": "",
+        /// "phone": "",
+        /// "address": "",
+        /// "sex": 0
+        /// }
+        /// </remarks>
+        /// <response code="200">刪除成功</response>
+        /// <response code="400">用戶名稱修改失敗</response>
         [HttpDelete("ApiDelete")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult ApiDelete([FromBody] DBClass data)
         {
             bool success = false;
--- Program.cs
+++ Program.cs
@@ -1,7 +1,37 @@
+using Microsoft.OpenApi;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API功能",
+        Description = "使用API進行新增修改刪除查詢(CRUD)資料，測試網頁在 https://localhost:7045/home/index",
+        TermsOfService = new Uri("https://example.com/terms"),  //服務條款
+        Contact = new OpenApiContact   // (聯絡資訊)
+        {
+            Name = "網站維護者 - 簡文翊",    // 顯示維護者或團隊的名稱（如："Example Contact"）。
+            Url = new Uri("https://https://github.com/seriousfoul/Wunyi-ASPNET-MVC-API-Swagger"),  // Url: 指向維護者的個人網站、專案頁面或聯絡表單的連結。
+            Email = "john27306666@yahoo.com.tw" //Email(選填)：可以直接加入聯絡信箱。 
+        },
+        License = new OpenApiLicense     // (授權資訊)
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";          // 產生專案名稱的xml
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));    // 指定xml的路徑為剛剛產生的xml
+});
 
 var app = builder.Build();
 
@@ -10,7 +40,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts();   
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;   // swagger會取代index.html
+    });
 }
 
 app.UseHttpsRedirection();
@@ -19,7 +58,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
